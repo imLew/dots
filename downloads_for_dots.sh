56e2ddea4f6b122576491225069da48bdda18d9c
Lew-Myshkin <kol@posteo.de>
2019-10-10 11:28:23 +0200

    nicer cloning of vim plugins

--- downloads_for_dots.sh
+++ downloads_for_dots.sh
@@ -3,28 +3,32 @@
 #install oh-my-zsh
 cd $HOME
 git clone https://github.com/imLew/oh-my-zsh.git 
-cd $HOME.oh-my-zsh
+cd $HOME/.oh-my-zsh
 git pull origin running:running
 git checkout running
 
 #install vim plugins
 echo "Downloading plugins for vim\n"
 cd $HOME/.vim/pack/plugins/opt/
-git clone https://github.com/Konfekt/FastFold.git
-git clone https://github.com/tmhedberg/SimpylFold.git
-git clone https://github.com/Valloric/YouCompleteMe.git
-git clone https://github.com/w0rp/ale.git
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/vim-airline/vim-airline
-git clone https://github.com/git@github.com:tpope/vim-fugitive.git
-git clone https://github.com/airblade/vim-gitgutter.git
-git clone https://github.com/sheerun/vim-polyglot.git
-git clone https://github.com/zhimsel/vim-stay.git
-git clone https://github.com/tpope/vim-surround.git
-git clone https://github.com/christoomey/vim-tmux-navigator.git
-git clone https://github.com/peitalin/vim-jsx-typescript.git
-git clone https://github.com/leafgarland/typescript-vim.git
-git clone https://github.com/dyng/ctrlsf.vim.git
-git clone https://github.com/ctrlpvim/ctrlp.vim.git
 
-
+PLUGIN_REPOS=(
+"https://github.com/Konfekt/FastFold.git"
+"https://github.com/tmhedberg/SimpylFold.git"
+"https://github.com/Valloric/YouCompleteMe.git"
+"https://github.com/w0rp/ale.git"
+"https://github.com/scrooloose/nerdtree.git"
+"https://github.com/vim-airline/vim-airline"
+"https://github.com/git@github.com:tpope/vim-fugitive.git"
+"https://github.com/airblade/vim-gitgutter.git"
+"https://github.com/sheerun/vim-polyglot.git"
+"https://github.com/zhimsel/vim-stay.git"
+"https://github.com/tpope/vim-surround.git"
+"https://github.com/christoomey/vim-tmux-navigator.git"
+"https://github.com/peitalin/vim-jsx-typescript.git"
+"https://github.com/leafgarland/typescript-vim.git"
+"https://github.com/dyng/ctrlsf.vim.git"
+"https://github.com/ctrlpvim/ctrlp.vim.git"
+)
+for rep in "${PLUGIN_REPOS[@]}"; do
+    git clone $rep
+done
