ed4f2476b01afa543772dd65732fdcb07da3d74f
lew <kol@posteo.de>
2019-11-06 13:26:54 +0100

    update vim plugin list

--- downloads_for_dots.sh
+++ downloads_for_dots.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/sh
 
 #install oh-my-zsh
 cd $HOME
@@ -19,22 +19,22 @@ if [ -d "$HOME/.vim/pack/plugins/opt/" ]; then
 	echo "Downloading plugins for vim\n"
 
 	PLUGIN_REPOS=(
-	"https://github.com/Konfekt/FastFold.git"
-	"https://github.com/tmhedberg/SimpylFold.git"
-	"https://github.com/Valloric/YouCompleteMe.git"
 	"https://github.com/w0rp/ale.git"
+	"https://github.com/ctrlpvim/ctrlp.vim.git"
+	"https://github.com/dyng/ctrlsf.vim.git"
+	"https://github.com/Konfekt/FastFold.git"
 	"https://github.com/scrooloose/nerdtree.git"
+	"https://github.com/tmhedberg/SimpylFold.git"
+	"https://github.com/leafgarland/typescript-vim.git"
 	"https://github.com/vim-airline/vim-airline"
-	"https://github.com/git@github.com:tpope/vim-fugitive.git"
+    "git@github.com:JuliaEditorSupport/julia-vim.git"
 	"https://github.com/airblade/vim-gitgutter.git"
+	"https://github.com/peitalin/vim-jsx-typescript.git"
 	"https://github.com/sheerun/vim-polyglot.git"
 	"https://github.com/zhimsel/vim-stay.git"
 	"https://github.com/tpope/vim-surround.git"
 	"https://github.com/christoomey/vim-tmux-navigator.git"
-	"https://github.com/peitalin/vim-jsx-typescript.git"
-	"https://github.com/leafgarland/typescript-vim.git"
-	"https://github.com/dyng/ctrlsf.vim.git"
-	"https://github.com/ctrlpvim/ctrlp.vim.git"
+	"https://github.com/Valloric/YouCompleteMe.git"
 	)
 	for rep in "${PLUGIN_REPOS[@]}"; do
 	    if [ ! -d "$rep" ]; then
