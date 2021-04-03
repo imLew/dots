c4d79647b5c4ac4d650ce27d2c6af7939dbe2964
lew <kol@posteo.de>
2021-04-03 23:46:23 +0200

    don't use own fork omz, download plugins

--- downloads_for_dots.sh
+++ downloads_for_dots.sh
@@ -3,15 +3,19 @@ DOTS="$(pwd)"
 
 #install oh-my-zsh
 cd $HOME
-git clone https://github.com/robbyrussell/oh-my-zsh.git
-mv oh-my-zsh .oh-my-zsh
-if [ -d "$HOME/.oh-my-zsh" ]; then
-	cd $HOME/.oh-my-zsh
-	git remote add myfork https://github.com/imLew/oh-my-zsh.git 
-	git pull myfork running:running
-	git checkout running
-	git merge master
-fi
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+# zsh plugins
+git clone https://github.com/popstas/zsh-command-time.git ~/.oh-my-zsh/custom/plugins/command-time
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+
+# if [ -d "$HOME/.oh-my-zsh" ]; then
+# 	cd $HOME/.oh-my-zsh
+# 	git remote add myfork https://github.com/imLew/oh-my-zsh.git 
+# 	git pull myfork running:running
+# 	git checkout running
+# 	git merge master
+# fi
 
 # install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
